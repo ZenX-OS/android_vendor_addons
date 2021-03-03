68ba8987ed19744edc25786303b1cf9cdef15ddf
ZeNiXxX <zenixxx.havoc@gmail.com>
2021-03-03 09:08:30 +0100

    addons: Cleanup and improve UI Corner styles
    Signed-off-by: ZeNiXxX <zenixxx.havoc@gmail.com>

--- themes/ui_corner/UiStyleRoundLarge/Android.mk
+++ themes/ui_corner/UiStyleRoundLarge/Android.mk
@@ -18,8 +18,8 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
 
-LOCAL_PACKAGE_NAME := UiStyleRectangle
-LOCAL_RRO_THEME := UiStyleRectangle
+LOCAL_PACKAGE_NAME := UiStyleRoundLarge
+LOCAL_RRO_THEME := UiStyleRoundLarge
 LOCAL_SDK_VERSION := current
 LOCAL_CERTIFICATE := platform
 LOCAL_PRODUCT_MODULE := true
